11b821b1c5271b649769f9b1a80285ccbf0e431f
SELVARAJUshunmugaPriyan <shanmu.thukaram@gmail.com>
2022-12-27 16:05:27 +0100

    hetnet gateway wifi interface simulation created

---
+++ hetnet-gw/scripts/wifi-IP.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+'This example shows how to create multiple interfaces in stations'
+
+import sys
+from mininet.log import setLogLevel, info
+from mn_wifi.cli import CLI
+from mn_wifi.net import Mininet_wifi
+from mn_wifi.link import wmediumd, adhoc
+from mn_wifi.wmediumdConnector import interference
+
+
+def topology():
+    "Create a network."
+    net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference)
+
+    info("*** Creating nodes\n")
+    sta1 = net.addStation('sta1', position='200,250,0')
+    sta2 = net.addStation('sta2', position='150,250,0')
+    sta3 = net.addStation('sta3', position='150,200,0')
+    sta4 = net.addStation('sta4', position='150,150,0')
+    sta5 = net.addStation('sta5', position='200,150,0')
+    ap1 = net.addAccessPoint('ap1', ssid='ssid_1', mode='g', channel='5',
+                                                     failMode="standalone", position='200,200,0')
+
+    info("*** Configuring Propagation Model\n")
+    net.setPropagationModel(model="logDistance", exp=4)
+
+    info("*** Configuring wifi nodes\n")
+    net.configureWifiNodes()
+
+    info("*** Associating...\n")
+    net.addLink(ap1, sta1)
+    net.addLink(ap1, sta2)
+    net.addLink(ap1, sta3)
+    net.addLink(ap1, sta4)
+    net.addLink(ap1, sta5)
+
+    net.plotGraph(max_x=300, max_y=300)
+
+    info("*** Starting network\n")
+    net.build()
+    net.addNAT().configDefault()
+    ap1.start([])
+
+    info("*** Running CLI\n")
+    CLI(net)
+
+    ap1.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
+    """
+    sta1.cmd("ip r a 198.51.100.100/32 via 10.0.0.6") # Change Destination and AP IP address ( ... is usually +1 to # of sta nodes)
+    sta2.cmd("ip r a 198.51.100.100/32 via 10.0.0.6")
+    sta3.cmd("ip r a 198.51.100.100/32 via 10.0.0.6")
+    sta4.cmd("ip r a 198.51.100.100/32 via 10.0.0.6")
+    sta5.cmd("ip r a 198.51.100.100/32 via 10.0.0.6")
+    """
+    
+    if len(sys.argv) > 1 :
+            info("*** Introducing loss ", sys.argv[2], "%\n")
+            sta1.cmd("tc qdisc add dev sta11-wlan0 root netem loss " + sys.argv[2] + "%")
+            sta2.cmd("tc qdisc add dev sta12-wlan0 root netem loss " + sys.argv[2] + "%")
+            sta3.cmd("tc qdisc add dev sta13-wlan0 root netem loss " + sys.argv[2] + "%")
+            sta4.cmd("tc qdisc add dev sta14-wlan0 root netem loss " + sys.argv[2] + "%")
+            sta5.cmd("tc qdisc add dev sta15-wlan0 root netem loss " + sys.argv[2] + "%")
+
+	
+    f = sys.argv[2] if len(sys.argv) > 1 else "0"
+
+    info("*** Running Test\n")
+    #sta1.cmd("ping -c 600 -s 1232 198.51.100.100 | tee log/A_W_6_" + f + "L_6x6_.log")
+    #sta22.cmd("ping -c 300 -s 1232 198.51.100.100 | tee log/A_W_6_0L_" + f + "x" + f + "_Stable.log")
+
+    
+    info("*** Stopping network\n")
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topology()
